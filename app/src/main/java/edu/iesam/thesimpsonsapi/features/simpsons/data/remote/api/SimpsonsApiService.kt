6b5659f6936233f664db2a0df60522659e0c28b2
crfj <franciscoj.cubros@educa.jcyl.es>
2025-10-31 16:15:20 +0100

    fix layout issues

--- app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/data/remote/api/SimpsonsApiService.kt
+++ app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/data/remote/api/SimpsonsApiService.kt
@@ -2,10 +2,10 @@ package edu.iesam.thesimpsonsapi.features.simpsons.data.remote.api
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SimpsonsApiService {
 
     @GET("characters")
-    suspend fun findAll(page: Int): Response<SimpsonApiModel>
+    suspend fun findAll(@Query("page")page: Int): Response<SimpsonApiModel>
 }

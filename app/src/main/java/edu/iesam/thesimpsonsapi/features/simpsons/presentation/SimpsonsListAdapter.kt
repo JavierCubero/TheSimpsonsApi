e33369c295621647b25abf00bb65516fcdc72744
crfj <franciscoj.cubros@educa.jcyl.es>
2025-10-31 16:26:25 +0100

    rename character.xml to view_item_character.xml

--- app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/presentation/SimpsonsListAdapter.kt
+++ app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/presentation/SimpsonsListAdapter.kt
@@ -27,7 +27,7 @@ class SimpsonsListAdapter(private val characters: List<Character>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.character, parent, false)
+            .inflate(R.layout.view_item_character, parent, false)
         return ViewHolder(view)
     }
 

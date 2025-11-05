6aaee74e2bb0181fadbd40dfc627e3d03ac7acea
crfj <franciscoj.cubros@educa.jcyl.es>
2025-11-05 15:45:11 +0100

    modify the SimpsonsMainActivity.kt class to change findviewbyid for viewBinding

--- app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/presentation/SimpsonsMainActivity.kt
+++ app/src/main/java/edu/iesam/thesimpsonsapi/features/simpsons/presentation/SimpsonsMainActivity.kt
@@ -8,15 +8,19 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import edu.iesam.thesimpsonsapi.R
+import edu.iesam.thesimpsonsapi.databinding.ActivityMainBinding
 
 
 class SimpsonsMainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
